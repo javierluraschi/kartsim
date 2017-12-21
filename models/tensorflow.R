2f19cef05070cd6f953c300a44deb9ffb6a208f0
Javier Luraschi <javierluraschi@hotmail.com>
2017-12-21 12:29:07 -0800

    add cifar based classification model

---
+++ models/tensorflow.R
@@ -0,0 +1,67 @@
+library(keras)
+library(tfruns)
+
+model <- keras_model_sequential()
+
+model %>%
+  # Start with hidden 2D convolutional layer being fed 32x32 pixel images
+  layer_conv_2d(
+    filter = 32, kernel_size = c(3,3), padding = "same", 
+    input_shape = c(32, 32, 3)
+  ) %>%
+  layer_activation("relu") %>%
+  
+  # Second hidden layer
+  layer_conv_2d(filter = 32, kernel_size = c(3,3)) %>%
+  layer_activation("relu") %>%
+  
+  # Use max pooling
+  layer_max_pooling_2d(pool_size = c(2,2)) %>%
+  layer_dropout(0.25) %>%
+  
+  # 2 additional hidden 2D convolutional layers
+  layer_conv_2d(filter = 32, kernel_size = c(3,3), padding = "same") %>%
+  layer_activation("relu") %>%
+  layer_conv_2d(filter = 32, kernel_size = c(3,3)) %>%
+  layer_activation("relu") %>%
+  
+  # Use max pooling once more
+  layer_max_pooling_2d(pool_size = c(2,2)) %>%
+  layer_dropout(0.25) %>%
+  
+  # Flatten max filtered output into feature vector 
+  # and feed into dense layer
+  layer_flatten() %>%
+  layer_dense(512) %>%
+  layer_activation("relu") %>%
+  layer_dropout(0.5)
+
+model %>%
+  # Outputs from dense layer are projected onto 10 unit output layer
+  layer_dense(3) %>%
+  layer_activation("softmax")
+
+opt <- optimizer_rmsprop(lr = 0.0001, decay = 1e-6)
+
+model %>% compile(
+  loss = "categorical_crossentropy",
+  optimizer = opt,
+  metrics = "accuracy"
+)
+
+library(png)
+classes <- c("left", "right", "forward")
+
+batch_size <- 10000
+
+model %>% fit_generator(
+  flow_images_from_directory(
+    "capture/test",
+    classes = classes,
+    batch_size = batch_size,
+    target_size = c(32, 32)),
+  steps_per_epoch = as.integer(50000 / batch_size), 
+  epochs = 2
+)
+
+model %>% export_savedmodel("savedmodel")
